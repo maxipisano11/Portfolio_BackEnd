b15e7e60a5ae38971569c48194da0522641a7a34
maxipisano11 <maxi.e.pisano@gmail.com>
2023-03-25 19:54:43 -0300

    Nueva compilación/directorio target

--- src/main/java/com/portfolio/maxi_pisano/Controller/ExperienciaController.java
+++ src/main/java/com/portfolio/maxi_pisano/Controller/ExperienciaController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explaboral")
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins={"http://localhost:4200", "https://frontmaxi.web.app/"})
 
 public class ExperienciaController {
     @Autowired
--- src/main/java/com/portfolio/maxi_pisano/Controller/PersonaController.java
+++ src/main/java/com/portfolio/maxi_pisano/Controller/PersonaController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins={"http://localhost:4200", "https://frontmaxi.web.app/"})
 public class PersonaController {
     @Autowired
     ImpPersonaService impPersonaService;
